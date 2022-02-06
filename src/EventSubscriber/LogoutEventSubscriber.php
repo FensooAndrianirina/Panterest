4382be4749314fd0625626f53f846fde36f3b796
Fenosoa Andrianirina <fensooandrian@gmail.com>
2022-02-06 22:58:41 +0200

    Edit flash message for the logout

--- src/EventSubscriber/LogoutEventSubscriber.php
+++ src/EventSubscriber/LogoutEventSubscriber.php
@@ -5,14 +5,18 @@ namespace App\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 class LogoutEventSubscriber implements EventSubscriberInterface
 {
     private $urlGenerator;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator)
+    private $security;
+
+    public function __construct( Security $security,  UrlGeneratorInterface $urlGenerator)
     {
+        $this ->security = $security;
         $this->urlGenerator = $urlGenerator;
     }
     
@@ -20,7 +24,7 @@ class LogoutEventSubscriber implements EventSubscriberInterface
     {
         $event->getRequest()->getSession()->getFlashBag()->add(
             'success',
-            'Logged out successfully!'
+            $this->security->getUser()->getFullName() . ", you've been successfully logged out!"
         );
         
         $event->setResponse(new RedirectResponse($this->urlGenerator->generate('app_login')));
